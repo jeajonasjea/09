4f87dcc4a599211a6111c6987e6126df0d4264b9
jea10 <jea10@DESKTOP-RDKTSLC>
2016-11-10 09:45:09 +0100

    Test program

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushyButton 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            while (true)
+            {
+                Console.WriteLine("Count: " + input);
+                Console.ReadKey(true);
+                input++;
+
+                if (input == 0)
+                    input = 0;  
+             // vores program kan kun tælle x'er
+             //skal lave ult + chargedult
+             if (xp = 10)      
+            }
+        }
+    }
+}
